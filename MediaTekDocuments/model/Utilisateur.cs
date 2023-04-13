c1340a05bda54f4a605c74cb7ae13c418e4bffed
oumbarae hachmahocine <hachmaoui@hotmail.fr>
2023-04-13 23:36:09 +0200

    Mission 4 : authentification

---
+++ MediaTekDocuments/model/Utilisateur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaTekDocuments.model
+{
+    public class Utilisateur
+    {
+
+        public string login { get; set; }
+        public string pwd { get; set; }
+
+        public Utilisateur(string login, string pwd)
+        {
+            this.login = login;
+            this.pwd = pwd;
+        }
+    }
+}
---
+++ MediaTekDocuments/view/Authentification.cs
@@ -0,0 +1,57 @@
+﻿using MediaTekDocuments.controller;
+using MediaTekDocuments.model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaTekDocuments.view
+{
+    public partial class Authentification : Form
+    {
+        private readonly BindingSource bdg = new BindingSource();
+        //private List<lesutilisateurs> lesprofils = new List<lesutilisateurs>();
+        private readonly FrmMediatekController controller;
+        public Authentification()
+        {
+            InitializeComponent();
+        }
+
+        private void btnvalider_Click(object sender, EventArgs e)
+        {
+            string login = txtlogin.Text;
+            string pwd = txtpwd.Text;
+            Utilisateur admin = new Utilisateur(login, pwd);
+            //lesprofils = controller.verifadmin(admin);
+            //if (lesprofils != null)
+            //{
+            //    bdg.DataSource = lesprofils;
+            //    dgvuti.DataSource = bdg;
+
+            //}
+            if (txtlogin.Text == "" || txtpwd.Text == "")
+            {
+                MessageBox.Show("Veuillez remplir tous les champs ");
+
+            }
+            else
+            {
+                if (txtlogin.Text == "admin" && txtpwd.Text == "pwd")
+                {
+                    FrmMediatek frm = new FrmMediatek();
+                    frm.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Erreur d'authentification ");
+                }
+            }
+        }
+    }
+}
